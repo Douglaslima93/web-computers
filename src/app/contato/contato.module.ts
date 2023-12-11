d9308e1260ddfbfb05d4acecc0ac356aaf84ebe3
Douglas Lima <douglas07a@gmail.com>
2023-12-11 12:23:35 -0300

    Validação da pagina contatos

--- src/app/contato/contato.module.ts
+++ src/app/contato/contato.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ContatoRoutingModule } from './contato-routing.module';
 import { ContatoComponent } from './contato.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask'
 
 
 @NgModule({
@@ -11,7 +12,9 @@ import { ContatoComponent } from './contato.component';
   ],
   imports: [
     CommonModule,
-    ContatoRoutingModule
+    ContatoRoutingModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot()
   ]
 })
 export class ContatoModule { }
